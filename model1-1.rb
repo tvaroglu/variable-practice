e6ee035f23c47cd4cc528c6e838c5f1771d26608
Taylor Varoglu <tava8398@colorado.edu>
2021-03-30 11:57:53 -0600

    Fix int & float example in model1-1.rb

--- model1-1.rb
+++ model1-1.rb
@@ -5,20 +5,20 @@ p string3
 
 num1 = 4.0
 multiple = 4
-product = 4 * 4
+product = num1 * multiple
 p product
 
 is_true = true
 is_false = false
-p is_true 
+p is_true
 p is_false
 
 int_arr = [1, 2, 3]
 str_arr = ['one', 'two', 'three']
-p int_arr 
+p int_arr
 p str_arr
 
 old_school_hash = {:key1=>'value1', :key2=>'value2'}
 json_hash = {key1: 'value1', key2: 'value2'}
-p old_school_hash 
+p old_school_hash
 p json_hash
